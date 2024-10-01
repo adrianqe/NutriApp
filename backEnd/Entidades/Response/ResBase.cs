26e07e60b475b200a5b50fbfc4242d4172874e90
Adrian Quiros <157768117+adrianqe@users.noreply.github.com>
2024-09-30 23:42:51 -0600

    Create LogUser

---
+++ backEnd/Entidades/Response/ResBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backEnd.Entidades
+{
+    public class ResBase
+    {
+        public bool exito { get; set; }
+        public List<string> mensaje { get; set; }
+    }
+}
