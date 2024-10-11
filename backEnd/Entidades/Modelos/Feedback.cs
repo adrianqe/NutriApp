ea503afeb0992603c162bfb37b89cfba35045638
Carlos Efrén Arce <157768129+CarlosArce2212@users.noreply.github.com>
2024-10-11 01:12:03 -0600

    Tambien

--- backEnd/Entidades/Modelos/Feedback.cs
+++ backEnd/Entidades/Modelos/Feedback.cs
@@ -13,5 +13,10 @@ namespace backEnd.Entidades
         public int? Calificacion { get; set; }
         public string Comentario { get; set; }
         public int? FeedbackID { get; set; }
+    }        public int? UsuarioID { get; set; }
+        public int? ProductoID { get; set; }
+        public int? Calificacion { get; set; }
+        public string Comentario { get; set; }
+        public int? FeedbackID { get; set; }
     }
 }
